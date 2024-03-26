795b47075b4e64da0eb41fbbb87a9d6098037537
admin <nhatquang2306@gmail.com>
2024-03-26 11:11:32 +0700

    check

--- src/cross_rerank/trainer.py
+++ src/cross_rerank/trainer.py
@@ -30,7 +30,7 @@ class RerankerTrainer(Trainer):
         #print(inputs)
         #print(inputs['input_ids'].size())
         outputs: SequenceClassifierOutput = model(inputs)
-        loss = outputs.loss['logits'].sum()
+        loss = outputs.loss
         #print(loss)
 
         if self.model.training:
